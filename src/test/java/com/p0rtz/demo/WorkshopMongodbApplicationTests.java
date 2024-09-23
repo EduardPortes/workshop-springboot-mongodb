85aafa7f48e9d2ab8afa9a5577ff4dd55fc7384d
Eduardo Portes <epcduas05@hotmail.com>
2024-09-23 12:37:04 -0300

    Primeiro commit

---
+++ src/test/java/com/p0rtz/demo/WorkshopMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p0rtz.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
