1b694df918b27539886cf8c736850308aeadaa5d
Eduardo Portes <epcduas05@hotmail.com>
2024-09-23 16:07:10 -0300

    Insercao de usuario com POST

--- src/main/java/com/p0rtz/demo/services/UserService.java
+++ src/main/java/com/p0rtz/demo/services/UserService.java
@@ -1,10 +1,13 @@
 package com.p0rtz.demo.services;
 
 import com.p0rtz.demo.domain.User;
+import com.p0rtz.demo.dto.UserDTO;
 import com.p0rtz.demo.repository.UserRepository;
 import com.p0rtz.demo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,8 +24,14 @@ public class UserService {
      public User findById(String id){
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+     }
 
+     public User insert(User obj){
+        return repo.insert(obj);
+     }
 
+     public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
      }
 
 }
