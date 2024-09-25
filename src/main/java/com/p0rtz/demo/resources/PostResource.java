6332d51924f443c7eb35ec9ce4da84939262a4f6
Eduardo Portes <epcduas05@hotmail.com>
2024-09-25 12:27:43 -0300

    Obtendo um post por id

---
+++ src/main/java/com/p0rtz/demo/resources/PostResource.java
@@ -0,0 +1,22 @@
+package com.p0rtz.demo.resources;
+
+import com.p0rtz.demo.domain.Post;
+import com.p0rtz.demo.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id){
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
---
+++ src/main/java/com/p0rtz/demo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.p0rtz.demo.services;
+
+import com.p0rtz.demo.domain.Post;
+import com.p0rtz.demo.repository.PostRepository;
+import com.p0rtz.demo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id){
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
